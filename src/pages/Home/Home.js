67e282169b11d1efefee3fa0d954856e48677a65
TienHunter <tienkbtnhp@gmail.com>
2022-09-13 17:27:42 +0700

    test fetch api nodejs

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,102 +1,104 @@
 
+import {useState,useEffect} from 'react'
 import { Link } from "react-router-dom"
 import classNames from "classnames/bind";
 import styles from "./Home.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
-
+import {removeAccents} from '../../utils/common'
 const cx = classNames.bind(styles);
 
-const animations = [
-   {
-      tilte: "Quân Hữu Vân",
-      link: "quan-huu-van",
-      image: "https://i0.wp.com/hhhkungfu.tv/wp-content/uploads/Quan-Huu-Van.jpg?fit=320%2C449&ssl=1",
-      children: [
-         {
-            episode: 1,
-            link: "link episode"
-         }
-      ]
-   },
-   {
-      tilte: "Đấu la đại lục",
-      link: "dau-la-dai-luc",
-      image: "https://i0.wp.com/hhhkungfu.tv/wp-content/uploads/2022/01/Dau-La-Dai-Luc.jpg?fit=320%2C449&ssl=1",
-   },
-   {
-      tilte: "Đấu phá thương khung",
-      link: "dau-pha-thuong-khung",
-      image: "https://i0.wp.com/hhhkungfu.tv/wp-content/uploads/2022/01/Dau-Pha-Thuong-Khung-OVA-3.jpg?fit=320%2C449&ssl=1",
-   },
-   {
-      tilte: "Quân Hữu Vân",
-      link: "link",
-      image: "https://i0.wp.com/hhhkungfu.tv/wp-content/uploads/Quan-Huu-Van.jpg?fit=320%2C449&ssl=1"
-   },
-   {
-      tilte: "Quân Hữu Vân",
-      link: "link",
-      image: "https://i0.wp.com/hhhkungfu.tv/wp-content/uploads/Quan-Huu-Van.jpg?fit=320%2C449&ssl=1"
-   },
-   {
-      tilte: "Quân Hữu Vân",
-      link: "link",
-      image: "https://i0.wp.com/hhhkungfu.tv/wp-content/uploads/Quan-Huu-Van.jpg?fit=320%2C449&ssl=1"
-   },
-   {
-      tilte: "Quân Hữu Vân",
-      link: "link",
-      image: "https://i0.wp.com/hhhkungfu.tv/wp-content/uploads/Quan-Huu-Van.jpg?fit=320%2C449&ssl=1"
-   },
-   {
-      tilte: "Quân Hữu Vân",
-      link: "link",
-      image: "https://i0.wp.com/hhhkungfu.tv/wp-content/uploads/Quan-Huu-Van.jpg?fit=320%2C449&ssl=1"
-   },
-   {
-      tilte: "Quân Hữu Vân",
-      link: "link",
-      image: "https://i0.wp.com/hhhkungfu.tv/wp-content/uploads/Quan-Huu-Van.jpg?fit=320%2C449&ssl=1"
-   },
-   {
-      tilte: "Quân Hữu Vân",
-      link: "link",
-      image: "https://i0.wp.com/hhhkungfu.tv/wp-content/uploads/Quan-Huu-Van.jpg?fit=320%2C449&ssl=1"
-   },
-   {
-      tilte: "Quân Hữu Vân",
-      link: "link",
-      image: "https://i0.wp.com/hhhkungfu.tv/wp-content/uploads/Quan-Huu-Van.jpg?fit=320%2C449&ssl=1"
-   },
-   {
-      tilte: "Quân Hữu Vân",
-      link: "link",
-      image: "https://i0.wp.com/hhhkungfu.tv/wp-content/uploads/Quan-Huu-Van.jpg?fit=320%2C449&ssl=1"
-   },
-   {
-      tilte: "Quân Hữu Vân",
-      link: "link",
-      image: "https://i0.wp.com/hhhkungfu.tv/wp-content/uploads/Quan-Huu-Van.jpg?fit=320%2C449&ssl=1"
-   },
-   {
-      tilte: "Quân Hữu Vân",
-      link: "link",
-      image: "https://i0.wp.com/hhhkungfu.tv/wp-content/uploads/Quan-Huu-Van.jpg?fit=320%2C449&ssl=1"
-   },
-   {
-      tilte: "Quân Hữu Vân",
-      link: "link",
-      image: "https://i0.wp.com/hhhkungfu.tv/wp-content/uploads/Quan-Huu-Van.jpg?fit=320%2C449&ssl=1"
-   }
+// const animations = [
+//    {
+//       tilte: "Quân Hữu Vân",
+//       link: "quan-huu-van",
+//       image: "https://i0.wp.com/hhhkungfu.tv/wp-content/uploads/Quan-Huu-Van.jpg?fit=320%2C449&ssl=1",
+//       children: [
+//          {
+//             episode: 1,
+//             link: "link episode"
+//          }
+//       ]
+//    },
+//    {
+//       tilte: "Đấu la đại lục",
+//       link: "dau-la-dai-luc",
+//       image: "https://i0.wp.com/hhhkungfu.tv/wp-content/uploads/2022/01/Dau-La-Dai-Luc.jpg?fit=320%2C449&ssl=1",
+//    },
+//    {
+//       tilte: "Đấu phá thương khung",
+//       link: "dau-pha-thuong-khung",
+//       image: "https://i0.wp.com/hhhkungfu.tv/wp-content/uploads/2022/01/Dau-Pha-Thuong-Khung-OVA-3.jpg?fit=320%2C449&ssl=1",
+//    },
+//    {
+//       tilte: "Quân Hữu Vân",
+//       link: "link",
+//       image: "https://i0.wp.com/hhhkungfu.tv/wp-content/uploads/Quan-Huu-Van.jpg?fit=320%2C449&ssl=1"
+//    },
+//    {
+//       tilte: "Quân Hữu Vân",
+//       link: "link",
+//       image: "https://i0.wp.com/hhhkungfu.tv/wp-content/uploads/Quan-Huu-Van.jpg?fit=320%2C449&ssl=1"
+//    },
+//    {
+//       tilte: "Quân Hữu Vân",
+//       link: "link",
+//       image: "https://i0.wp.com/hhhkungfu.tv/wp-content/uploads/Quan-Huu-Van.jpg?fit=320%2C449&ssl=1"
+//    },
+//    {
+//       tilte: "Quân Hữu Vân",
+//       link: "link",
+//       image: "https://i0.wp.com/hhhkungfu.tv/wp-content/uploads/Quan-Huu-Van.jpg?fit=320%2C449&ssl=1"
+//    },
+//    {
+//       tilte: "Quân Hữu Vân",
+//       link: "link",
+//       image: "https://i0.wp.com/hhhkungfu.tv/wp-content/uploads/Quan-Huu-Van.jpg?fit=320%2C449&ssl=1"
+//    },
+//    {
+//       tilte: "Quân Hữu Vân",
+//       link: "link",
+//       image: "https://i0.wp.com/hhhkungfu.tv/wp-content/uploads/Quan-Huu-Van.jpg?fit=320%2C449&ssl=1"
+//    },
+//    {
+//       tilte: "Quân Hữu Vân",
+//       link: "link",
+//       image: "https://i0.wp.com/hhhkungfu.tv/wp-content/uploads/Quan-Huu-Van.jpg?fit=320%2C449&ssl=1"
+//    },
+//    {
+//       tilte: "Quân Hữu Vân",
+//       link: "link",
+//       image: "https://i0.wp.com/hhhkungfu.tv/wp-content/uploads/Quan-Huu-Van.jpg?fit=320%2C449&ssl=1"
+//    },
+//    {
+//       tilte: "Quân Hữu Vân",
+//       link: "link",
+//       image: "https://i0.wp.com/hhhkungfu.tv/wp-content/uploads/Quan-Huu-Van.jpg?fit=320%2C449&ssl=1"
+//    },
+//    {
+//       tilte: "Quân Hữu Vân",
+//       link: "link",
+//       image: "https://i0.wp.com/hhhkungfu.tv/wp-content/uploads/Quan-Huu-Van.jpg?fit=320%2C449&ssl=1"
+//    },
+//    {
+//       tilte: "Quân Hữu Vân",
+//       link: "link",
+//       image: "https://i0.wp.com/hhhkungfu.tv/wp-content/uploads/Quan-Huu-Van.jpg?fit=320%2C449&ssl=1"
+//    },
+//    {
+//       tilte: "Quân Hữu Vân",
+//       link: "link",
+//       image: "https://i0.wp.com/hhhkungfu.tv/wp-content/uploads/Quan-Huu-Van.jpg?fit=320%2C449&ssl=1"
+//    }
 
-]
+// ]
 function HomePage() {
-   // const navigate = useNavigate();
-   // const onDesFilm = (data) => {
-   //    console.log("check linkURL: ", data);
-   //    navigate(data);
-   // }
+   const [films, setFilms] = useState([])
+   useEffect(() => {
+      fetch('http://localhost:8080/api/films')
+  .then((response) => response.json())
+  .then((data) =>setFilms(data));
+   },[])
    return (
       <div className={cx("wrapper")}>
          <div className={cx("grid")}>
@@ -108,7 +110,7 @@ function HomePage() {
                      </div>
                   </h4>
                </div>
-               {animations.map((animation, index) => {
+               {films.map((film, index) => {
                   // let linkURL = animation.link;
                   return (
                      <div
@@ -116,12 +118,12 @@ function HomePage() {
                         key={index}
                      // onClick={() => onDesFilm(linkURL)}
                      >
-                        <Link className={cx("item")} to={animation.link}>
-                           <img className={cx("image")} src={animation.image} alt="avatar" />
-                           {animation.children ? <span className={cx("episode")}>{`Tập ${animation.children[animation.children.length - 1].episode}`}</span> : ''}
+                        <Link className={cx("item")} to={removeAccents(film.filmName)}>
+                           <img className={cx("image")} src={'http://localhost:8080/' + film.image} alt="avatar" />
+                           {/* {animation.children ? <span className={cx("episode")}>{`Tập ${animation.children[animation.children.length - 1].episode}`}</span> : ''} */}
                            <div className={cx("overlay")}></div>
                            <div className={cx("titel-box")}>
-                              <div className={cx("title")}>{animation.tilte}</div>
+                              <div className={cx("title")}>{film.filmName}</div>
                            </div>
                         </Link>
                      </div>
